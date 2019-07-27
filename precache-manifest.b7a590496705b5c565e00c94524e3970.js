bbed499ffdb647fd965b0e324ca969d6479ca263
Nirmal201 <ninu201@gmail.com>
2019-07-27 01:55:37 -0400

    Updates

---
+++ precache-manifest.b7a590496705b5c565e00c94524e3970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49c47cd246f33dfb2b2f1546b2bbfdb",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "4501f7afe5855efbf6c8",
+    "url": "/react-tutorial/static/css/main.251767c1.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "4501f7afe5855efbf6c8",
+    "url": "/react-tutorial/static/js/main.a6fa40db.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
